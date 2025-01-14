a8d5392d5657bc5e837685fc89c62c995508b13b
viduladakshitha <viduladakshitha@gmail.com>
2025-01-14 14:49:18 +0530

    added url change

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -6,7 +6,7 @@
 
 // export const remote_url_v1 = "http://95.216.206.214:8000/api/v1/"
 
-export const remote_url_v1 = "http://localhost:8000/api/v1/"
+export const remote_url_v1 = "https://sparetan-main-be-dtfdckafeebqg2er.northeurope-01.azurewebsites.net/api/v1/"
 
 export const remote_chat_url_v1 = "ws://95.216.206.214:8001/"
 
